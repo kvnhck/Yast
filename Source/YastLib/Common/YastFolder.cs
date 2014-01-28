9233b7fbdabb84b2eded9f6e4139ba2b79f7e5d7
Kevin Van Haecke <kevin@novado.be>
2014-01-28 22:15:26 +0100

    Added YastFolder en YastPrivilege class

---
+++ Source/YastLib/Common/YastFolder.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace YastLib.Common
+{
+    public class YastFolder
+    {
+        private readonly XElement _folder;
+
+        public int Id
+        {
+            get { return _folder.GetElementValue("id", 0); }
+        }
+
+        public string Name
+        {
+            get { return _folder.GetElementValue("name", string.Empty); }
+        }
+
+        public string Description
+        {
+            get { return _folder.GetElementValue("description", string.Empty); }
+        }
+
+        public string PrimaryColor
+        {
+            get { return _folder.GetElementValue("primaryColor", string.Empty); }
+        }
+
+        public int ParentId
+        {
+            get { return _folder.GetElementValue("parentId", 0); }
+        }
+
+        public YastPrivilege Privileges
+        {
+            get { return (YastPrivilege) _folder.GetElementValue("privileges", 0); }
+        }
+
+        public DateTime TimeCreated
+        {
+            get { return YastTime.FromSecondsSince1970(_folder.GetElementValue("timeCreated", 0D)); }
+        }
+
+        public int CreatorId
+        {
+            get { return _folder.GetElementValue("creator", 0); }
+        }
+
+        public YastFolder(XElement folder)
+        {
+            _folder = folder;
+        }
+    }
+}
---
+++ Source/YastLib/Common/YastPrivilege.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace YastLib.Common
+{
+    [Flags]
+    public enum YastPrivilege
+    {
+        None = 0,
+        Owner = 1,
+        Read = 2,
+        Modify = 4,
+        Delete = 8,
+        //Reserved = 16,
+        WorkerRole = 32,
+        ProjectManagerRole = 64,
+        AdministratorRole = 128
+    }
+}
