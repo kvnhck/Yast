5d36259230ca9092eafe2e4f580477676356b37d
Kevin Van Haecke <kevin@novado.be>
2014-01-28 21:55:05 +0100

    Added XElement extension methods

---
+++ Source/YastLib/Common/XElementExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace YastLib.Common
+{
+    public static class XElementExtensions
+    {
+        public static T GetElementValue<T>(this XElement element, string name, T defaultValue)
+        {
+            var xElement = element.Element(name);
+            if (xElement == null) return defaultValue;
+
+            return (T)Convert.ChangeType(xElement.Value, typeof(T));
+        }
+
+        public static T GetAttributeValue<T>(this XElement element, string name, T defaultValue)
+        {
+            var xAttribute = element.Attribute(name);
+            if (xAttribute == null) return defaultValue;
+
+            return (T)Convert.ChangeType(xAttribute.Value, typeof(T));
+        }
+    }
+}
